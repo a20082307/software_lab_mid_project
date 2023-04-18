9e8c5cd43a0f52e2010473cb7f565f13927cce35
110062333 <rogerline0527@gmail.com>
2023-04-19 03:23:22 +0800

    import firebase, it can now register and sign in with text email and password, but still not for Google and Facebook

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import firebase from 'firebase/compat/app'
 
-
-export default function Login() {
+export default function Login(props) {
     const [txtEmail, setEmail] = useState('')
     const [txtPassword, setPassword] = useState('')
+    let [emailInput, setEmailInput] = useState()
+    let [passwordInput, setPasswordInput] = useState()
+
     const handleInput = (event) => {
         const {name, value} = event.target
         if (name === 'txtEmail') 
@@ -13,14 +16,26 @@ export default function Login() {
     }
 
     const handleLogIn = () => {
-        // firebase.auth().signInWithEmailAndPassword(txtEmail, txtPassword)
-        //     .then((userCredential) => {
-        //         var user = userCredential.user
-        //     })
-        //     .catch(e => alert(e.message));
+        firebase.auth().createUserWithEmailAndPassword(txtEmail, txtPassword)
+        .then( userCredential => {
+            //var user = userCredential.user;
+            alert('success')
+        })
+        .catch( error => 
+            alert(error.message)    
+        )
+
+        emailInput.value = passwordInput.value = ''
     }
-    const handleSignOut = () => {
-        // firebase.auth().signOut()
+    const handleSignUp = () => {
+        firebase.auth().signInWithEmailAndPassword(txtEmail, txtPassword)
+        .then( userCredential => {
+            //var user = userCredential.user
+            alert('success')
+        })
+        .catch( error => 
+            alert(error.message)    
+        )
     }
 
     return (
@@ -30,15 +45,17 @@ export default function Login() {
                 type = 'text' 
                 name = "txtEmail"
                 placeholder = 'Please input Email'
+                ref = {node => setEmailInput(node)}
             ></input><br/>
             <input 
                 onChange={handleInput} 
                 type = 'password' 
                 name = 'txtPassword'
                 placeholder = 'Please input Password'
+                ref = {node => setPasswordInput(node)}
             ></input><br/>
-            <button type = 'submit' onClick = {handleLogIn}>Log in</button>
-            <button type = 'submit' onClick = {handleSignOut}>Sign up</button><br/>
+            <button onClick = {handleSignUp}>Sign up</button><br/>
+            <button onClick = {handleLogIn}>Log in</button><br/>
             <button>Google</button>
             <button>Facebook</button>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/database'
 
 import App from './App'
 
+const config = {
+    apiKey: "AIzaSyCWnE61qwD_Sq4vsyx9Rrjs32cJJ_UjZls",
+    authDomain: "mid-project-597a7.firebaseapp.com",
+    projectId: "mid-project-597a7",
+    storageBucket: "mid-project-597a7.appspot.com",
+    messagingSenderId: "385018300200",
+    appId: "1:385018300200:web:dad04b67d5d1cf99ea321d",
+    measurementId: "G-G8RTS5Q35B"
+}
+const app = firebase.initializeApp(config)
+
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(<App/>)
+
+
