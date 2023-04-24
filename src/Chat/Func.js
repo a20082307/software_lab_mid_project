2d2d644c6a5daa5477ff3c22f2d1c9be6ea533e2
110062333 <rogerline0527@gmail.com>
2023-04-25 03:13:57 +0800

    Day 1-some part of basic Roughly decide what should be store in firebase Probably finish creating a new chatroom Probably finish joining an existed chatroom Finish loading the newest chatroom list from firebase when login Slightly change the scss of third-party button

--- src/Chat/Func.js
+++ src/Chat/Func.js
@@ -1,18 +1,104 @@
 import React, { useState, useEffect } from 'react'
 
+
 import newImg from '../img/new.png'
 import joinImg from '../img/join.png'
 import logoutImg from '../img/logout.png'
 
 export default function Func(props) {
-    const {firebase, feature, setHasLogin, hasLogin} = props
+    const {firebase, feature, user} = props
+    const {setHasLogin, chatNum, setChatNum} = props
+    const {chatList, setChatList} = props
 
     const NewChatroom = () => {
+        let chatName
+        let leave = false
+        while(true) {
+            chatName = prompt('Please input the name of Chatroom', `${user.displayName}'s Chatroom`)
+            if (chatName == null) {
+                alert('Cancel creating a Chatroom')
+                return
+            }
 
+            if (chatName == '') 
+                continue;
+
+            break
+        }
+        firebase.database().ref().orderByChild('chatName').equalTo(chatName).once('value', snapshot => {
+            if (snapshot.exists()) 
+                alert('The name has been repeated!')
+            else {
+                firebase.database().ref(`chat-${chatNum}`).set({
+                    nth: chatNum,
+                    chatName: chatName,
+                    chatId: cyrb53(`${user.displayName}'s Chatroom`, chatNum),
+                    message: [`${user.displayName} has joined the room`]
+                })
+                
+                let temList = chatList
+                temList.push(chatNum)
+                setChatList(temList)
+                firebase.database().ref(`${user.uid}`).update(chatList)
+
+                alert('create chatroom!')
+            }
+        })
     }
 
     const JoinChatroom = () => {
+        let target = prompt('Please input the name or the #id of Chatroom!')
+        if (target[0] === '#') {
+            let targetId = parseInt(target.substring(1))
 
+            if (targetId != NaN) {
+                firebase.database().ref().orderByChild('chatId').equalTo(targetId).once('value', snapshot => {
+                    if (snapshot.exists()) {
+                        let targetChatroom = Object.values(snapshot.val())[0]
+                        if (chatList.some(element => element.chatId === targetChatroom.chatId)) {
+                            alert('already in this room')
+                            return
+                        }
+
+                        targetChatroom.message.push(`${user.displayName} has joined the room`)
+                        setChatList([...chatList, targetChatroom])
+
+                        let temList = chatList
+                        temList.push(targetChatroom.nth)
+                        setChatList(temList)
+                        firebase.database().ref(`${user.uid}`).update(chatList)
+
+                        alert('join the chatroom!')
+                        
+                        firebase.database().ref(`chat-${targetChatroom.nth}`).update(targetChatroom)
+                    }
+                    else 
+                        alert('no such room id')
+                })
+            }
+            else 
+                alert('Please input id after #')
+        }
+        else {
+            firebase.database().ref().orderByChild('chatName').equalTo(target).once('value', snapshot => {
+                if (snapshot.exists()) {
+                    let targetChatroom = Object.values(snapshot.val())[0]
+                    if (chatList.some(element => element.chatId === targetChatroom.chatId)) {
+                        alert('already in this room')
+                        return
+                    }
+
+                    targetChatroom.message.push(`${user.displayName} has joined the room`)
+                    setChatList([...chatList, targetChatroom])
+                    alert('join the chatroom!')
+                    
+                    firebase.database().ref(`chat-${targetChatroom.nth}`).update(targetChatroom)
+                }
+                else 
+                    alert('no such room name')
+            })
+        }
+        console.log(chatList)
     }
 
     const Logout = () => {
@@ -30,12 +116,6 @@ export default function Func(props) {
             alert(error.message)
         })
     }
-
-    // useEffect(() => {
-    //     document.documentElement.style.setProperty('--show-title', !hasLogin ? 'none' : 'block')
-    //     document.documentElement.style.setProperty('--show-login', !hasLogin ? 'none' : 'block')
-    //     document.documentElement.style.setProperty('--animate', !hasLogin ? 'rotate' : 'none')
-    // }, [hasLogin])
 
     let img, handleEvent
     switch (feature) {
@@ -59,4 +139,18 @@ export default function Func(props) {
         </button>
     )
 }
-// new, join, logout
+
+const cyrb53 = (str, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'firebase/compat/app'
+import React, {useState, useEffect} from 'react'
 import User from './User'
 import Func from './Func'
 
@@ -7,13 +7,26 @@ import RoomList from './RoomList'
 
 export default function Chat(props) {
     const {firebase, user, setHasLogin, hasLogin} = props
+    const [chatNum, setChatNum] = useState(0)
+    const [chatList, setChatList] = useState([])
+
+    useEffect(() => {
+        firebase.database().ref(`${user.uid}`).once('value', snapshot => {
+            if (snapshot.exists())
+                setChatList(snapshot.val())
+            else {
+                setChatList([])
+                console.log('empty')
+            }
+        })
+    }, [])
 
     return (
         <div id = 'chat-container'>
             <User {...props}/>
             <RoomList {...props}/>
-            <Func {...props} feature = {'new'} />
-            <Func {...props} feature = {'join'} />
+            <Func {...props} feature = {'new'} chatNum = {chatNum} setChatNum = {setChatNum} chatList = {chatList} setChatList = {setChatList} />
+            <Func {...props} feature = {'join'} chatList = {chatList} setChatList = {setChatList} />
             <Func {...props} feature = {'logout'} setHasLogin = {setHasLogin} hasLogin = {hasLogin} />
             <input type = 'text' placeholder = 'message'></input>
         </div>
