fbc6c6e20f9431a59f7cb1d4e0b9865fa58c046b
givemeyourhatplease <rogerline0527@gmail.com>
2023-04-27 03:04:32 +0800

    Revert "has title and input area, but still can't login since haven't use fir…"

--- src/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../scss/title.style.scss'
-
-export default function Title() {
-    return (
-        <h2 id = 'login-title'>Online Chat</h2>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-import React, { StrictMode } from 'react'
-import ReactDom from 'react-dom/client'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import 'firebase/compat/database'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App from './App'
-import './scss/index.style.scss'
-
-const config = {
-    apiKey: "AIzaSyCWnE61qwD_Sq4vsyx9Rrjs32cJJ_UjZls",
-    authDomain: "mid-project-597a7.firebaseapp.com",
-    projectId: "mid-project-597a7",
-    storageBucket: "mid-project-597a7.appspot.com",
-    messagingSenderId: "385018300200",
-    appId: "1:385018300200:web:dad04b67d5d1cf99ea321d",
-    measurementId: "G-G8RTS5Q35B"
-}
-firebase.initializeApp(config)
-
-const root = ReactDom.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <StrictMode>
-        <App firebase = {firebase}/>
-    </StrictMode>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
